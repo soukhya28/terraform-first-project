c248a67dbad5bd19d4842c40d032b4ccd726cfcb
soukhya28 <soukhya468@gmail.com>
2025-07-16 10:03:42 +0000

    lastest-commit

--- resource.tf
+++ resource.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "storeterra" {
 }
  
 resource "azurerm_storage_account" "souterra" {
-    name                     = "terrastoragesou286"
+    name                     = "terrastoragesoureddy286"
     resource_group_name      = azurerm_resource_group.storeterra.name
     location                 = azurerm_resource_group.storeterra.location
     account_tier             = "Standard"
