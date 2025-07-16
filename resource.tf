670f1ed0a84db7b785d9db707198719fb5755c72
soukhya28 <soukhya468@gmail.com>
2025-07-16 10:13:17 +0000

    latest-commit

--- resource.tf
+++ resource.tf
@@ -19,20 +19,20 @@ resource "azurerm_storage_container" "soucontainer" {
 resource "azurerm_virtual_network" "terra_vnet" {
     name                = "terra-vnet"
     address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terra.location
+    location            = azurerm_resource_group.storeterra.location
     resource_group_name = azurerm_resource_group.terra.name
 }
  
 resource "azurerm_subnet" "subnet1" {
     name                 = "subnet1"
-    resource_group_name  = azurerm_resource_group.terra.name
+    resource_group_name  = azurerm_resource_group.storeterra.name
     virtual_network_name = azurerm_virtual_network.terra_vnet.name
     address_prefixes     = ["10.0.1.0/24"]
 }
  
 resource "azurerm_subnet" "subnet2" {
     name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.terra.name
+    resource_group_name  = azurerm_resource_group.storeterra.name
     virtual_network_name = azurerm_virtual_network.terra_vnet.name
     address_prefixes     = ["10.0.2.0/24"]
 }
