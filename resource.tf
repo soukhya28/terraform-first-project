d7c5415c88a126c56842bfef253d91f13a22d0ee
soukhya28 <soukhya468@gmail.com>
2025-07-16 09:27:35 +0000

    last-commit

--- resource.tf
+++ resource.tf
@@ -1,5 +1,20 @@
-resource "azurerm_resource_group" "terra" {
-  name     = "terra"
-  location = "East US"
+resource "azurerm_resource_group" "storeterra" {
+    name     = "terraresources"
+    location = "East US"
 }
+ 
+resource "azurerm_storage_account" "souterra" {
+    name                     = "terrastorage286"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "soucontainer" {
+    name                  = "terra-container"
+    storage_account_name  = azurerm_storage_account.saterra.name
+    container_access_type = "private"
+}
+
  
