886706b90e53d60b79d5c21956088ebdcc405218
soukhya28 <soukhya468@gmail.com>
2025-07-16 10:14:23 +0000

    latest-commit

--- resource.tf
+++ resource.tf
@@ -20,7 +20,7 @@ resource "azurerm_virtual_network" "terra_vnet" {
     name                = "terra-vnet"
     address_space       = ["10.0.0.0/16"]
     location            = azurerm_resource_group.storeterra.location
-    resource_group_name = azurerm_resource_group.terra.name
+    resource_group_name = azurerm_resource_group.storeterra.name
 }
  
 resource "azurerm_subnet" "subnet1" {
